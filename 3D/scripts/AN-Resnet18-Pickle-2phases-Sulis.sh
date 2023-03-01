042724acf38a10a5c704cadfc87323aca2a98c1c
Quangminh <94977232+Quickflash777@users.noreply.github.com>
2023-03-01 15:28:13 +0000

    Update AN-Resnet18-Pickle-2phases-Sulis.sh

--- 3D/scripts/AN-Resnet18-Pickle-2phases-Sulis.sh
+++ 3D/scripts/AN-Resnet18-Pickle-2phases-Sulis.sh
@@ -361,11 +361,12 @@ cat > ${job} << EOD
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=3700
-#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task=42
+#SBATCH --mem-per-cpu=3850
+#SBATCH --gres=gpu:ampere_a100:1
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:quadro_rtx_6000:1
+#SBATCH --time=48:00:00
+#SBATCH --account=su007-rr-gpu
 
 module purge
 module restore PT
