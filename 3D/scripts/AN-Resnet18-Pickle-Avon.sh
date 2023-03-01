e8e530ed4146244d5781408fff9f391cdfd49011
Quangminh <94977232+Quickflash777@users.noreply.github.com>
2023-03-01 15:24:50 +0000

    Update and rename AvonGPU.sh to AN-Resnet18-Pickle-Avon.sh

--- 3D/scripts/AN-Resnet18-Pickle-Avon.sh
+++ 3D/scripts/AN-Resnet18-Pickle-Avon.sh
@@ -5,8 +5,9 @@ re=${3:-0}
 no=${4:-4000}
 size=${5:-30}
 
-echo "Version:" `git describe --tags --long` echo "Branch:" `git branch --show-current`
-echo $getseed $no $size $epochs $re
+echo $getseed $epochs $re $no $size 
+
+
 
 workdir=$(pwd)
 
@@ -39,24 +40,20 @@ echo "Current time : $now"
 cat > ${py} << EOD
 #!/usr/bin/env python
 # coding: utf-8
-
 print("--> importing modules")
 import os, shutil, pathlib
 import torch
 print("--> torch version used = 1.7.1, version loaded = " + str(torch.__version__))
 import pandas as pd
-
 from torch import nn
 from torch.utils.data import DataLoader, Dataset, ConcatDataset
 from torch.utils.data import random_split
 from torchvision import models
 from datetime import datetime
-
-
 import numpy as np
+import pickle
 import time
 import random
-
 import matplotlib
 import matplotlib.pyplot as plt
 print("--> matplotlib version used = 3.3.3, version loaded = " + str(matplotlib.__version__))
@@ -67,25 +64,20 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 print("--> sklearn version used = 0.23.2, version loaded = " + str(sklearn.__version__))
-
 print("--> import complete")
-
 print(datetime.now())
 print("$getseed $epochs $re $no $size")
-
 class CustomImageDataset(Dataset):
     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
         self.img_labels = pd.read_csv(annotations_file, nrows=$no)
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
-
     def __len__(self):
         return len(self.img_labels)
-
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = np.genfromtxt(f"{img_path}")
+        image = np.load(img_path, allow_pickle=True)
         image = np.square(image)
         image = image.reshape(1,$size,$size,$size)
         label = self.img_labels.iloc[idx, 1]
@@ -94,26 +86,17 @@ class CustomImageDataset(Dataset):
         if self.target_transform:
             label = self.target_transform(label)
         return image, label
-
 print("--> defining categories")
-
 c = np.loadtxt("$cdir")
 print(c)
-
-
 casez = []
 for i in range (0, len(c)):
     casez = np.append(casez, "W"+str(c[i]))
 print(casez)
-
 print("--> categories have been defined. No. of categories used = " + str(len(c)))
-
 store="N$no-L$size"
-
 torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = True
-
-
 if "$getseed" != "N":
 	print("--> loading previous trial seed")
 	f = open("$workdir/lastseed.txt", "r")
@@ -125,17 +108,13 @@ if "$getseed" != "N":
 else:
 	seed = torch.seed()
 	random.seed(seed)
-
 f = open("$workdir/lastseed.txt", "w")
 f.write(str(seed))
 print("current seed: " + str(seed))
 f.close()
-
 print("--> seed saved to lastseed.txt in $workdir")
-
 path = pathlib.Path("$numdir")
 os.chdir(path)
-
 print("--> creating labels file to identify files to be used")
 if os.path.exists(f"{path}/labels"):
     shutil.rmtree(f"{path}/labels")
@@ -144,16 +123,10 @@ for i in range(0,len(casez)):
     csv_input = pd.read_csv(f'{path}/{casez[i]}/labels.csv')
     csv_input.replace(to_replace=0,value=i,inplace = True)
     csv_input.to_csv(f'{path}/labels/labels{c[i]}.csv', index=False)
-
-
 src = os.listdir(f'{path}/labels')
 a = pd.concat([pd.read_csv(f'{path}/labels/{file}') for file in src ], ignore_index=True)
 a.to_csv(f'{path}/labels/labels.csv', index=False)
-
 print("--> created labels file")
-
-
-
 ###################################
 print("--> creating datasets for usage for training, validation and testing")
 batch_size = 32
@@ -174,34 +147,22 @@ print("--> created datasets")
 print("--> training set contains " + str(len(training_data)) + " files")
 print("--> validation set contains " + str(len(validation_data)) + " files")
 print("--> test set contains " + str(len(test_data)) + " files")
-
-
-
 # Create data loaders.
-
 train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
 validation_dataloader = DataLoader(validation_data, batch_size=batch_size, shuffle=True)
 test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
-
-
-
-
 train_features, train_labels = next(iter(train_dataloader))
 print(f"Feature batch shape: {train_features.size()}")
 print(f"Labels batch shape: {train_labels.size()}")
 img = train_features[0].squeeze()
 label = train_labels
 print(f"Label: {label}")
-
-
-
 # Get cpu or gpu device for training.
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
-
 print("--> preparing model from resnet18 network")
 model = models.video.r3d_18()
-model.stem[0] = nn.Conv3d(in_channels=1, out_channels=64, kernel_size=(3,3,3), stride=(1,1,1), padding=(1,1,1),dilation=(1,1,1), bias=False)
+model.stem[0] = nn.Conv3d(in_channels=1, out_channels=64, kernel_size=(3,3,3), stride=(1,1,1), padding=(1,1,1), dilation=(1,1,1), bias=False)
 model.fc = nn.Linear(in_features=512,out_features=len(c),bias=True)
 if $re != 0:
 	for i in range(0,$re):
@@ -212,23 +173,19 @@ if torch.cuda.is_available():
     model.cuda()
 print("--> model defined for use")
 print(model)
-
 ################################
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0117395, eps=4.5765e-08, weight_decay=0.000138954)
+optimizer = torch.optim.Adam(model.parameters())
 print(f"optimizer used: {optimizer}")
-
-
 epochs = $epochs - $re
 if $re == 0:
     if os.path.exists(f"$workdir/saved models"):
         shutil.rmtree(f"$workdir/saved models")
     os.mkdir(f"$workdir/saved models")
-# torch.save(model.state_dict(), f"$workdir/saved models/saved_model[{$re}].pth")
+torch.save(model.state_dict(), f"$workdir/saved models/saved_model[{$re}].pth")
 min_valid_loss = np.inf
 tl = np.array([])
 vl = np.array([])
-
 print("--> beginning training")
 for e in range(epochs):
     st = time.time()
@@ -240,14 +197,10 @@ for e in range(epochs):
         
         optimizer.zero_grad()
         target = model(data.float())
-        
-
-
-    loss = loss_fn(target,labels)
-    loss.backward()
-    optimizer.step()
-    train_loss += loss.item()
-
+        loss = loss_fn(target,labels)
+    	loss.backward()
+    	optimizer.step()
+    	train_loss += loss.item()
     print("--> creating training confusion matrix")
     predict = []
     p = []
@@ -275,9 +228,7 @@ for e in range(epochs):
         if torch.cuda.is_available():
             vdata, vlabels = vdata.cuda(), vlabels.cuda()
         
-        vtarget = model(vdata.float())
-        
-        
+        vtarget = model(vdata.float())     
         vloss = loss_fn(vtarget,vlabels)
         valid_loss = vloss.item() * data.size(0)
     print("--> creating training confusion matrix")
@@ -302,7 +253,6 @@ for e in range(epochs):
     np.savetxt(f"$workdir/cm-valid-C{len(c)}-D$size-{e+$re}.csv", vcm, delimiter=",")
     et = time.time()
     rt = et-st
-
     print(f'Epoch {e+1+$re} \t Runtime: {round(rt,2)}s \t Training Loss: {train_loss / len(train_dataloader)} \t Validation Loss: {valid_loss / len(validation_dataloader)}')
     # if min_valid_loss > valid_loss:
         # print(f'Validation Loss Decreased({min_valid_loss:.6f}--->{valid_loss:.6f}) \t Saving The Model')
@@ -319,12 +269,10 @@ for e in range(epochs):
     f.write(str(tl) + "\n")
     print(f"--> stored training loss values: {tl}")
     f.close()
-
     f = open("$workdir/vl.txt", "a+")
     f.write(str(vl) + "\n")
     print(f"--> stored validation loss values: {vl}")
     f.close()
-
     print("--> testing model against test data (to see model accuracy)")
     tpredict = []
     tp = []
@@ -340,38 +288,28 @@ for e in range(epochs):
             tpredicted = torch.Tensor.cpu(tpredicted)
             tpredict = np.append(tpredict, tpredicted)
             tp = np.append(tp, tactual)
-
-
     print("--> computing confusion matrix")
     tcm = confusion_matrix(tp, tpredict)
     print(tcm)
     print("--> saving confusion matrix")
     np.savetxt(f"$workdir/cm-test-C{len(c)}-D$size-{e+$re}.csv", tcm, delimiter=",")
-
-
 print("--> task complete")
 EOD
 
 
 cat > ${job} << EOD
 #!/bin/bash
-
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=3700
-#SBATCH --time=48:00:00
-#SBATCH --partition=gpu
+#SBATCH --cpus-per-task=42
+#SBATCH --mem-per-cpu=3850
 #SBATCH --gres=gpu:quadro_rtx_6000:1
-
+#SBATCH --partition=gpu
+#SBATCH --time=48:00:00
 module purge
 module restore PT
 module list
-
-
 srun $py
-
-
 EOD
 
 chmod 755 ${job}
@@ -379,4 +317,3 @@ chmod g+w ${job}
 chmod 755 ${py}
 
 sbatch ${job}
-
