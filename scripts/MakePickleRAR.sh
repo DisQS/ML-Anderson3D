9d85a55191dcb0271199b1e630dde7134cf8a26c
RudoRoemer <r.roemer@warwick.ac.uk>
2023-07-14 10:23:58 +0100

    rewrite to make it work with new batch of data

--- scripts/MakePickleRAR.sh
+++ scripts/MakePickleRAR.sh
@@ -1,44 +1,41 @@
 #!/bin/bash
 
-dir=${1:-../data}
-destdir=${2:-../data2}
-size=${3:-10}
-nb_sample=${4:-5000}
-cores=${5:-1}
+rawdir=${1:-../data}
+pkldir=${2:-../data2}
+nb_sample=${3:-10}
+cores=${4:-1}
 
 MAKEDISQS=/storage/disqs/MakeDisQS.sh
 
 nb_sample_raw=5000
 
 codedir=`pwd`
-pkl_folder="L"$size"-"$nb_sample"-pkl/"
-raw_folder="L"$size"-"$nb_sample_raw
-echo $pkl_folder
-echo "PERCO: dir=" $dir ", size=" $size ",nb_sample=" $nb_sample ", cores=" $cores
 
-# --- working in RAW directories
+echo "PERCO: rawdir=" $rawdir ", nb_sample=" $nb_sample ", cores=" $cores
 
-cd $dir
-cd "L"$size"-"$nb_sample_raw
+mkdir -p $pkldir
+
+# --- working in RAWDIR directories
+cd $rawdir
+#cd "L"$size"-"$nb_sample_raw
 pwd
 
-for directory in W*/
+for Wdir in A3*
 do
-    if [ ! -d $destdir"/"$pkl_folder\$directory ]; then
-        echo "--- making "$destdir"/"$pkl_folder$directory
-        mkdir -p $destdir"/"$pkl_folder$directory
-        #cd $destdir"/"$pkl_folder$directory
+    if [ ! -d $pkldir\$Wdir ]; then
+        echo "--- making "$pkldir/$Wdir
+        mkdir -p $pkldir/$Wdir
     else
-        echo "--- cd "$destdir"/"$pkl_folder$directory
-        #cd $destdir"/"$pkl_folder$directory
+        echo "--- cd "$pkldir/$Wdir
+        #cd $pkldir"/"$pkl_folder$Wdir
     fi
 
-    jobfile=MakePickle-$size".sh"
-    pyscript=MakePickle-$size".py"
+    jobfile=MakePickle-`basename $rawdir`".sh"
+    pyscript=MakePickle-`basename $rawdir`".py"
 
-    echo "--- creating "$jobfile $pyscript "in "$destdir"/"$pkl_folder$directory
+    echo "--- creating "$jobfile + $pyscript "in "$pkldir/$Wdir
 
-cat > $destdir"/"$pkl_folder$directory"/"${pyscript} << EOD
+cat > $pkldir/$Wdir/${pyscript} << EOD
 #!/usr/bin/python3
 import sys
 import numpy as np
@@ -47,20 +44,29 @@ import pickle as pkl
 
 #print(sys.argv)
 
-filename, file_extension = os.path.splitext(str(sys.argv[1]))
+filepath = os.path.dirname(str(sys.argv[3]))
+basename = os.path.basename(str(sys.argv[3]))
+#print('fp:', filepath)
+#print('bn:', basename)
+
+filename, file_extension = os.path.splitext(basename)
 print('>-- creation of file',filename+'.pkl')
 
-new_path=str(sys.argv[2])+filename+'.pkl'
+raw_path=str(sys.argv[1])+filepath[1:]+'/'+filename+'.raw'
+pkl_path=str(sys.argv[2])+'/'+filename+'.pkl'
+
+#print('raw: ',raw_path)
+#print('pkl: ',pkl_path)
 
 #print(sys.argv[1])
-data=np.loadtxt(str(sys.argv[1]))
+data=np.loadtxt(str(raw_path))
 
-pickle_file = open(new_path,"wb")
+pickle_file = open(pkl_path,"wb")
 pkl.dump(data, pickle_file)
 pickle_file.close()
 EOD
 
-cat > $destdir/$pkl_folder$directory"/"${jobfile} << EOD
+cat > $pkldir/$Wdir"/"${jobfile} << EOD
 #!/bin/bash
 #SBATCH --time=48:00:00
 #SBATCH --nodes=1
@@ -68,18 +74,22 @@ cat > $destdir/$pkl_folder$directory"/"${jobfile} << EOD
 #SBATCH --cpus-per-task=24
 #SBATCH --mem-per-cpu=3700
 
+# DT2018
+#module load GCCcore/11.2.0 parallel/20210722
+#module load Anaconda3
+# DT2022
 module load GCCcore/11.2.0 parallel/20210722
-module load Anaconda3
+module load Python/3.9.6
 
 #conda init --all; conda activate
 
-echo "--> working in directory=$dir/$raw_folder/$directory ---"
+echo "--> working in Wdir=$rawdir/$Wdir"
 
-cd $dir/$raw_folder/$directory
+cd $rawdir/$Wdir
 pwd
 rm -f raw.lst
 
-for rawfile in \`ls *.raw | head -$nb_sample\`
+for rawfile in \`find . -name \Evec*.raw | head -$nb_sample\`
 do
     if [ ! -f \`basename \$rawfile .raw\`.pkl ]; then
 	echo \$rawfile "needs .pkl file"
@@ -90,35 +100,35 @@ done
 
 pwd
 
-sort -R raw.lst | parallel -j$cores -a - python $destdir/$pkl_folder/$directory/$pyscript {} $destdir"/"$pkl_folder$directory
+sort -R raw.lst | parallel -j$cores -a - python $pkldir/$Wdir/$pyscript $rawdir/$Wdir $pkldir/$Wdir {} 
 
-#if [ ! -f $dir/$raw_folder/$directory/raw.lst ]; then
+#if [ ! -f $rawdir/$raw_folder/$Wdir/raw.lst ]; then
 #    echo "all the files are already converted to pkl"
 #else
-#    echo "the directory already exists"
-#    sort -R raw.lst | parallel -j$cores -a - python $pyscript {} $pkl_folder"/"$directory
+#    echo "the Wdir already exists"
+#    sort -R raw.lst | parallel -j$cores -a - python $pyscript {} $Wdir
 #fi
 
 pwd
 
-echo "--> finished in directory=$directory"
+echo "--> finished in Wdir=$Wdir"
 
 EOD
 
     #cat ${jobfile}
     #cat ${pyscript}
 
-    chmod 755 $destdir"/"$pkl_folder$directory"/"${jobfile}
-    chmod g+w $destdir"/"$pkl_folder$directory"/"${jobfile}
+    chmod 755 $pkldir"/"$Wdir"/"${jobfile}
+    chmod g+w $pkldir"/"$Wdir"/"${jobfile}
 
     echo "--> starting the .sh file"
     #(sbatch -q devel ${jobfile})
     #(sbatch -q taskfarm ${jobfile})
     #(sbatch ${jobfile})
-    (${destdir}"/"$pkl_folder$directory"/"${jobfile})
+    (${pkldir}"/"$pkl_folder$Wdir"/"${jobfile})
     
 done
 
 cd $codedir
 
-$MAKEDISQS $destdir"/"$pkl_folder
+$MAKEDISQS $pkldir"/"$pkl_folder
