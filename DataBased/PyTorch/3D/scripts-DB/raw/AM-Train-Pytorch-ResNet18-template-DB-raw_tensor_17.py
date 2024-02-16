2f9adea18beef4e4f0a4724669ca92281aa8b49a
Djena Bayo <phrhmb@mistral.theory.warwick.ac.uk>
2024-02-16 11:58:52 +0000

    Changed testpath

--- DataBased/PyTorch/3D/scripts-DB/raw/AM-Train-Pytorch-ResNet18-template-DB-raw_tensor_17.py
+++ DataBased/PyTorch/3D/scripts-DB/raw/AM-Train-Pytorch-ResNet18-template-DB-raw_tensor_17.py
@@ -58,7 +58,7 @@ print('###############')
 print(subclasses)
 dataname_og='L'+str(width)+'-'+str(size_data)+'-pkl'
 dataname='L'+str(width)+'-'+str(my_size_samp)+'-pkl'
-data_test='L'+str(width)+'-500-test'
+data_test='L'+str(width)+'-500-pkl-test'
 datapath = '/home/physics/phsht/Projects/ML-Anderson3D/Data/EvecPKL/'+dataname_og
 testpath = '/home/physics/phsht/Projects/ML-Anderson3D/Data/'
 print(os.listdir(datapath))
